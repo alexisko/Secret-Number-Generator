fd2d71a97f2a657dac52dd9b7fe9f6c07a8c39a6
Alexis <alexisko@hawaii.edu>
2017-05-18 18:55:12 -1000

    All tests passing

--- secret_number.js
+++ secret_number.js
@@ -1,4 +1,14 @@
-'use strict';
+//'use strict';
 module.exports = function() {
-  
+	var secretNumber = 0;
+	return function() {
+		if(secretNumber === 0) {
+			secretNumber = Math.random() * (1000000 - 0) + 0;
+		}
+		return secretNumber;
+	};
 };
+
+
+
+
